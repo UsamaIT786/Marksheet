bbc4d549675fc5eddc2b88ec46394879e66472ce
UsamaIT786 <160235187+UsamaIT786@users.noreply.github.com>
2024-03-12 16:24:01 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var username = "Usama Muzammil";
+username = "Usama Muzammil";
+console.log("Name", username);
+var number = 3302819536;
+number = 3302819536;
+console.log("phone number", number);
+//Marksheet
+console.log("Marksheet");
+var numbers = 454296;
+number = 454296;
+console.log("rollnumber", numbers);
+var English = 90;
+var IT = 91;
+var Urdu = 78;
+var Islamiat = 90;
+var Computerscience = 96;
+var Total = 600;
+var obtain = English + Urdu + Islamiat + IT + Computerscience;
+var Percentage = obtain / Total * 100;
+console.log("Total", obtain);
+console.log("percentage", Percentage);
+if (Percentage >= 80 && Percentage > 100)
+    console.log("Grade:", "A+");
+else if (Percentage >= 70 && Percentage > 100)
+    console.log("Grade:", "A");
+else if (Percentage >= 60 && Percentage > 100)
+    console.log("Grade:", "B");
+else if (Percentage >= 50 && Percentage > 100)
+    console.log("Grade:", "C");
---
+++ main.ts
@@ -0,0 +1,29 @@
+let username: string = "Usama Muzammil";
+username = "Usama Muzammil";
+console.log(`Name`,username);
+let number = 3302819536;
+number = 3302819536;
+console.log(`phone number`,number);
+//Marksheet
+console.log(`Marksheet`);
+let numbers: number =   454296;
+number = 454296;
+console.log(`rollnumber`,numbers)
+let English = 90;
+let IT = 91;
+let Urdu = 78;
+let Islamiat = 90;
+let Computerscience = 96;
+let Total = 600;
+let obtain = English+Urdu+Islamiat+IT+Computerscience;
+let Percentage = obtain/Total*100;
+console.log(`Total`,obtain);
+console.log(`percentage`,Percentage);
+if(Percentage>=80 && Percentage >100)
+console.log(`Grade:` , "A+");
+else if(Percentage>=70 && Percentage >100)
+console.log(`Grade:` , "A");
+ else if(Percentage>=60 && Percentage >100)
+console.log(`Grade:` , "B");
+else if(Percentage>=50 && Percentage >100)
+console.log(`Grade:` , "C");
